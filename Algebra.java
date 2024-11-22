67aed3d9c6247d6d1ed45ba3e561fd0ced2ac12d
yotam levy <yotam.levy01@post.runi.ac.il>
2024-11-22 15:27:42 +0200

    done

--- Algebra.java
+++ Algebra.java
@@ -1,4 +1,4 @@
-// Implements algebraic operations and the square root function without using 
+
 // the Java operations a + b, a - b, a * b, a / b, a % b, and without calling 
 // Math.sqrt. All the functions in this class operate on int values and
 // return int values.
@@ -25,43 +25,108 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int y1 = x1;
+		int y2 = x2;
+		for (int i = 0; i < y1; i++) {
+			y2 ++;
+		}
+		return y2;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int y1 = x1;
+		int y2 = x2;
+		for (int i = 0; i < y2; i++) {
+			y1 --;
+		}
+		return y1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int y1 = x1;
+		int y2 = x2;
+		int s = 0;
+		for (int i = 0; i < y2; i++) {
+			s = plus(s, y1);
+			
+		}
+		
+		return s;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int y = x;
+		int m = n;
+		int r = 0;
+		
+			if (m == 0) {
+				r = 1;
+			}
+			else {
+				r = y;
+			}
+		for (int i = 1; i < m; i++) {
+			r = times(r, y);
+		}
+		
+		return r;
 	}
 
 	// Returns the integer part of x1 / x2 
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int y1 = x1;
+		int y2 = x2;
+		int count = 0;
+
+		if (y2 == 0) {
+			throw new IllegalArgumentException("Division by zero is not allowed");
+
+		}
+		while (y1 >= y2) {
+			y1 = minus(y1, y2);
+			count ++;
+			
+		}
+			
+		return count;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int y1 = x1;
+		int y2 = x2;
+		int s = y1;
+		if (x2 == 0) {
+			throw new IllegalArgumentException("Division by zero is not allowed");
+		}
+
+		if (y1 < y2) {
+			s = y1;
+		} else {
+			
+		while (s >= y2) {
+			s = minus(s, y2);
+		}
+	}
+		return s;
 	}	
 
 	// Returns the integer part of sqrt(x) 
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
+		int r = 0;
+		int s = 0;
+
+		if (x < 0) {
+			throw new IllegalArgumentException("Cannot calculate the square root of a negative number");
+		}
+	
+		while (s <= x) {
+			r++;
+			s = times(r, r); 
+		}
+		return minus(r, 1);
 	}	  	  
 }
